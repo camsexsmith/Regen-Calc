eeb282f05f96100ab247335850d3dfdc68fecedf
Cameron <sexsmith.cameron@gmail.com>
2024-01-03 12:29:33 -0500

    Added classes & data
    organized variables in a better fashion. added data for RP1. updated README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Regen-Calc
  
+ This is going to do rocket engine regen cooling calcs.
+ We'll see how it goes I guess.
+
+ CEA DATA
+ Data taken from a CEA run with LOX-RP1 400 PSIA Chamber Pressure and O/F = 1.5 with frozen conditions
+
+ ENGINE CHARACTERISTICS
+ Engine values are very arbitrary currently. Just values that "look" good enough at this point. Will revist
+
+ HEAT TRANSFER
+ Using the Bartz Equation for calculation of the hot gas film coefficient. Not ideal for calculation of the chamber since it's meant primarily for the heat transfer in the nozzle section. I mean, it's heat transfer, it's going to be wrong no matter what equation you use. Might want tot switch to an equation with less variables pertaining to the nozzle parameters.
--- main.py
+++ main.py
@@ -7,36 +7,60 @@ Q_ = UR.Quantity
 siEnergy = "joule/kilogram/degK"
 enEnergy = "Btu/pound/degR"
 
-Pr = 0.525
-Mc = 0.1
-gam = 1.25
-Twg_Tc = 1
-At_Ac = 0.1
-
-pc = Q_(400,"pound/inch**2")
-cstar = Q_(5000,"feet/second")
-
-Dt = Q_(5,"centimeter") #throat diameter
-dt = Dt.to("inch")
-
-R = Q_(1,"centimeter")
-r = R.to("inch")
-
-Visc = Q_(3,"Pa*second")
-visc = Visc.to("pound/inch/second")
-
-Cond = Q_(3.27,"milliwatts/centimeter/degK")
-cond = Cond.to("Btu/second/inch**2/degF*inch")
-
-Cp = Q_(2.23,"kilojoule/kilogram/degK")
-cp = Cp.to("Btu/pound/degF")
-
 g = Q_(32.2,"feet/second**2")
 
-sig = 1/((0.5*Twg_Tc*(1 + ((gam-1)/2)*Mc**2) + 0.5)**0.68*(1 + ((gam-1)/2)*Mc**2)**0.12)
+# Engine operating conditions
+class Engine:
+    pc = Q_(400,"pound/inch**2")
+    cstar = Q_(5000,"feet/second")
+    Dt = Q_(5,"centimeter") #throat diameter
+    dt = Dt.to("inch")
+    R = Q_(1,"centimeter")
+    r = R.to("inch")
+    At_Ac = 0.1
+    wallThick = 5 * UR.millimeter
+    wallK = Q_(16.3,"watt/meter/degK")
 
-hg = (0.026/dt**0.2)*(visc**0.2*cp/Pr**0.6)*(pc*g/cstar)**0.8*(dt/r)**0.1*(At_Ac)**0.9*sig
+# Combustion gas values taken from CEA
+class Comb:
+    Visc = Q_(3,"Pa*second")    #CEA combustion viscosity
+    visc = Visc.to("pound/inch/second")
+
+    Cond = Q_(3.27,"milliwatts/centimeter/degK")    #CEA combustion conductivity
+    cond = Cond.to("Btu/second/inch**2/degF*inch")
+
+    Cp = Q_(2.23e3,"joule/kilogram/degK")     #CEA combustion Cp
+    cp = Cp.to("Btu/pound/degF")
+    Pr = 0.525  #Combustion Pr
+    Mc = 0.1    #Arbitrary chamber Mach (fix later)
+    gam = 1.25  #Combustion gas gamma
+
+    Twg_Tc = 1  #Arbitrary hot wall to combustion temp ratio
+    
+
+# Coolant (RP-1)
+class Coolant:
+
+    cond = Q_(0.078,"Btu/feet/hour/degF")
+    Cond = cond.to("watt/meter/degK")
+    visc = Q_(100e5,"pound/feet/second")
+    Visc = visc.to("pascal*second")
+    cp = Q_(0.49,"Btu/pound/degF")
+    Cp = cp.to("joule/kilogram/degK")
+
+    Pr = Visc*Cp/Cond
+
+
+# Analysis
+sig = 1/((0.5*Comb.Twg_Tc*(1 + ((Comb.gam-1)/2)*Comb.Mc**2) + 0.5)**0.68*(1 + ((Comb.gam-1)/2)*Comb.Mc**2)**0.12)
+
+hg = (0.026/Engine.dt**0.2)*(Comb.visc**0.2*Comb.cp/Comb.Pr**0.6)*(Engine.pc*g/Engine.cstar)**0.8*(Engine.dt/Engine.r)**0.1*(Engine.At_Ac)**0.9*sig
 
 Hg = hg.to("watt/meter**2/degK")
 
-print(Hg)
+Tc0 = 300 * UR.degK
+
+T0 = 2500 * UR.degK
+
+#qpp = (T0 - Tc0)/((1/Hg) + (wallThick/wallK) + (1/))
+print(Coolant.Visc)
